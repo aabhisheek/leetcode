b00bd0584fd7293fcbecf11df90b3833b91d4feb
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-07-07 23:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (82.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,71 +1,84 @@
-#include<string.h>
 class Solution {
 public:
+         
+
     int romanToInt(string s) {
-        int Roman(0);
-        for (int i = 0; i < s.length(); i++)
-        {
-            switch (s[i])
-            {
-            case 'I': 
-                if (s[i + 1] == 'V'||s[i+1]=='X')
-                {
-                    Roman -= 1;
-                    break;
-                }
-                else
-                {
-                    Roman += 1;
-                    break;
-                }
-            case 'V':
-            {
-                Roman += 5;
-                break;
-            }
-            case 'X':
-            {
-                if (s[i + 1] == 'L' || s[i + 1] == 'C')
-                {
-                    Roman -=10;
-                    break;
-                }
-                else
-                {
-                    Roman += 10;
-                    break;
-                }
-            }
-            case 'L':
-            {
-                Roman += 50;
-                break;
-            }
-            case 'C':
-            {
-                if (s[i + 1] == 'D' || s[i + 1] == 'M')
-                {
-                    Roman -= 100;
-                    break;
-                }
-                else
-                {
-                    Roman += 100;
-                    break;
-                }
-            }
-            case 'D':
-            {
-                Roman += 500;
-                break;
-            }
-            case 'M':
-            {
-                Roman +=1000;
-                break;
-            }
-            }
-        }
-        return Roman;
+
+
+ int ans =0;
+ int st =0;
+  int e = s.length();
+
+   while(st<e){
+
+if (s[st] == 'I' && s[st+1] != 'V' && s[st+1]!='X'){
+     ans= ans+1;
+     st++;
+}
+
+if (s[st]=='I' && s[st+1]=='V'){
+    ans = ans+4;
+   st= st+2;
+}
+
+if (s[st]=='I' && s[st+1]=='X'){
+    ans = ans+9;
+    st=st+2;
+}
+
+
+if (s[st] == 'X' && s[st+1] != 'L' && s[st+1]!='C'){
+     ans= ans+10;
+     st++;
+
+}
+
+if (s[st]=='X' && s[st+1]=='L'){
+    ans = ans+40;
+    st=st+2;
+}
+
+if (s[st]=='X' && s[st+1]=='C'){
+    ans = ans+90;
+    st=st+2;
+}
+
+if (s[st] == 'C' && s[st+1] != 'D' && s[st+1]!='M'){
+     ans= ans+100;
+     st++;
+}
+
+if (s[st]=='C' && s[st+1]=='D'){
+    ans = ans+400;
+    st=st+2;
+}
+
+if (s[st]=='C' && s[st+1]=='M'){
+    ans = ans+900;
+    st=st+2;
+}
+
+if (s[st]=='V'){
+    ans = ans+5;
+    st++;
+}
+
+if (s[st]=='L'){
+    ans = ans+50;
+    st++;
+}
+
+if (s[st]=='D'){
+    ans = ans+500;
+    st++;
+}
+
+if (s[st]=='M'){
+    ans = ans+1000;
+    st++;
+}
+  }
+
+  return ans ;
     }
 };
