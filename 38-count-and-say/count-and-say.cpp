c64ce4bb28f2f2e847ef49345e9345d294b84e51
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-07-09 11:58:21 +0530

    Time: 2 ms (74.51%) | Memory: 8.1 MB (91.57%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string num = "1";
-        while (--n > 0) {
-            num = countAndSay(num);
-        }
-        return num;
+          string num="1";
+          while(n>=2)
+          {
+            num=rle(num);
+            n--;
+          }
+          return num;  
     }
 
-private:
-    string countAndSay(const string& num) {
-        ostringstream sb;
-        int count, n = num.length();
-        for (int i = 0, j = 0; i < n; i = j) {
-            while (j < n && num[j] == num[i]) {
-                j++;
+     string rle(string num)
+    {   
+        int i=0;int count=0;string ans="";char ch;
+        while(i<num.size())
+        {    
+            ch='\0'; 
+            if((i<=num.size()-2)&&(num[i]==num[i+1]))
+            {  
+                count=1;ch='\0';
+                while((i<=num.size()-2)&&(num[i]==num[i+1]))
+                {
+                      count++;
+                      i++;
+                }
+                 ch='0'+count;
+                ans.push_back(ch);
+                ch=num[i];
+                ans.push_back(ch);
+                i++;
             }
-            sb << (j - i) << num[i];
+            else
+            { 
+                ch='1';
+                ans.push_back(ch);
+                ch=num[i];
+                ans.push_back(ch); 
+                i++;
+            }
+
+            
         }
-        return sb.str();
-    }
+        return ans;
+    } 
+
+
 };
