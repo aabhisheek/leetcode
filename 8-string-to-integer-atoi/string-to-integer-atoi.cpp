3914afd7c2d1e1825d3cc5765d2cfa8feeba77a9
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-07-17 12:17:53 +0530

    Time: 7 ms (12.11%) | Memory: 9.1 MB (11.84%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int num=0, i=0, sign=1;
-        //here we have taken nums as the final answer, 
-        //i for iterating the string and sign as positive 
-
-        //firstly, ignore the blank spaces
-        while(s[i]==' '){
+    int myAtoi(string s) 
+    {
+        int i=0;
+        int sign=1;
+        long ans=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-')
+        {
+            sign=-1;
             i++;
         }
-
-        //secondly, we will find a sign and we will multiply by sign at the 
-        //end
-        if(i<s.size() && (s[i]=='-' || s[i]=='+')){
-            sign = s[i]=='+'? 1:-1;
-            ++i;
-        }
-        //thirdly, we will traverse through digits
-        while(i<s.size() && isdigit(s[i])){
-            //out of bound
-            if(num > INT_MAX/10 || (num == INT_MAX/10 && s[i]>'7')){
-                return sign == -1? INT_MIN : INT_MAX;
+        else if(s[i]=='+')
+            i++;
+        while(i<s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+            {
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign==-1)
+                    return INT_MIN;
+                else if(ans>INT_MAX && sign==1)
+                    return INT_MAX;
+                i++;
             }
-
-            num=num*10+ (s[i]-'0');
-            ++i;
+            else
+                return ans*sign;
         }
-        return num*sign;
+        return (ans*sign);
     }
 };
