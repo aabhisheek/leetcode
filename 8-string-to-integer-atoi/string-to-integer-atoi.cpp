fb7d476f53b759b6c44ede527d498f73b73d774c
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-07-17 12:20:10 +0530

    Time: 3 ms (57.96%) | Memory: 9.2 MB (8.73%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,65 @@
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
-            i++;
-        }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
+    long long myAtoi(string s) {
+        long long num=0;int ans=0;
+        for(int i=0;i<s.size();i++)
+        {  
+            if((i==0)&&(s[i]==' ')) //whitespace handling
+             {
+                while(s[i]==' '){
                 i++;
+                }
+                ans=i;
+             }
+
+            if(i==ans)   //signhandling1
+            {
+            if(s[i]=='-')
+             continue; 
+             if(s[i]=='+')
+             continue;
+
             }
-            else
-                return ans*sign;
+
+             
+            
+            if((s[i]>='0')&&(s[i]<='9'))//formingnumber
+            {    
+                 num=num*10+(s[i]-'0');//forming the number
+                 cout<<num<<endl;
+
+                
+                if(num>INT_MAX)//overflow handling
+                {
+                    if(s[ans]=='-')//signhandling2a
+                    {   
+                        cout<<num;
+                        return INT_MIN;
+                    }
+                    else
+                    {
+                        cout<<num;
+                        return INT_MAX;
+                    }
+                }
+                   
+
+                // num=num*10+(s[i]-'0');//forming the number
+                // cout<<num<<endl;
+           
+            }
+            else 
+             break;
+              
         }
-        return (ans*sign);
+        if(s[ans]=='-')//signhandling2b
+        {
+            num=-num;
+        }
+         
+         
+         cout<<num;
+         return num;
     }
+
 };
