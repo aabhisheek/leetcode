169bfd21d5d10a74f1fba83d0a88fab3cdd0891d
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-07-19 12:32:36 +0530

    Time: 74 ms (74.53%) | Memory: 63.9 MB (50.29%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,18 +3,19 @@ public:
     int findDuplicate(vector<int>& nums) {
         int fast = 0;
         int slow = 0;
-        do
-        {
-            fast = nums[fast];
-            fast = nums[fast];
-            slow  = nums[slow];
-        }while(fast != slow);
-
+        
+        while (true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if (fast == slow)
+                break;
+        }
+        
         fast = 0;
-        do{
+        while (fast != slow) {
             fast = nums[fast];
             slow = nums[slow];
-        }while(fast != slow);
+        }
 
         return fast;
     }
