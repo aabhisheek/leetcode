0f229c1343c9fbe440f724219376665f62848dd7
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-09-11 23:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (90.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -26,7 +26,7 @@ public:
          memo[1]=1; 
          memo[2]=2;
         
-
+        //bottom up approach with   root elements as 0 and 1  
         for (int i = 3; i <= n; i++) {
             memo[i] = memo[i-1] + memo[i-2];
         }
@@ -51,12 +51,27 @@ public:
 
 
         //dp bottom up appraoch 
-        vector <int> memo(45+1,-1);
-        int ans=dp(n,memo);
-        return ans;
+        // vector <int> memo(45+1,-1);
+        // int ans=dp(n,memo);
+        // return ans;
 
 
-        
+         //space optimization by keeping only 2 pointers  prev and curr
+         if(n<=2)
+         return n;
+         
+
+         int prevprev=1;
+         int prev=1;
+         int curr=2;
+         for(int i=3;i<=n;i++)
+         {  
+            prevprev=curr;  
+            curr=prev+curr;
+            prev=prevprev;
+
+         }
+         return curr;
 
             
     }
