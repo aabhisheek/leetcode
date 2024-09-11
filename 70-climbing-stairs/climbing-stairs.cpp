0a7a5e74e1639fda719944ea34f8493354f4735d
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-09-11 23:29:14 +0530

    Time: 2 ms (49.48%) | Memory: 7.6 MB (50.15%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -14,6 +14,24 @@ public:
 
         return memo[n]=recursion(n-1,memo)+recursion(n-2,memo);
     }
+
+
+     int  dp(int n,vector <int> &memo)
+    {
+
+        if(memo[n]!=-1)
+        return memo[n];
+
+
+         memo[1]=1; 
+         memo[2]=2;
+        
+
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        return memo[n];
+    }
     
     
     int climbStairs(int n) {
@@ -26,22 +44,18 @@ public:
 
 
         // recursion  plus memoize
+        // vector <int> memo(45+1,-1);
+        // int ans=recursion(n,memo);
+        // return ans;
+
+
+
+        //dp bottom up appraoch 
         vector <int> memo(45+1,-1);
-        
-        // if(memo[n]!=-1)
-        // return memo[n];
-
-
-        // if(n<=2)
-        // return n;
-
-        
-
-        // return memo[n]=climbStairs(n-1)+climbStairs(n-2);
-
-        int ans=recursion(n,memo);
-
+        int ans=dp(n,memo);
         return ans;
+
+
         
 
             
