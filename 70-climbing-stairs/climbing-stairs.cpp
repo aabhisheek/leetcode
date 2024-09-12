306acfe42a24d4f7f620a64bacc6029c2c3adc7e
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2024-09-12 09:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (69.95%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,79 +1,26 @@
 class Solution {
 public:
-    int  recursion(int n,vector <int> &memo)
+    int recursion(int n,vector<int> &memo)
     {
+         if(memo[n]!=-1)
+         return memo[n];
 
-        if(memo[n]!=-1)
-        return memo[n];
-
-
-        if(n<=2)
-        return n;
-
-        
-
-        return memo[n]=recursion(n-1,memo)+recursion(n-2,memo);
-    }
-
-
-     int  dp(int n,vector <int> &memo)
-    {
-
-        if(memo[n]!=-1)
-        return memo[n];
-
-
-         memo[1]=1; 
-         memo[2]=2;
-        
-        //bottom up approach with   root elements as 0 and 1  
-        for (int i = 3; i <= n; i++) {
-            memo[i] = memo[i-1] + memo[i-2];
-        }
-        return memo[n];
-    }
-    
-    
-    int climbStairs(int n) {
-        //recursion
-        
-        // if(n<=2)
-        // return n;
-
-        // return climbStairs(n-1)+climbStairs(n-2);
-
-
-        // recursion  plus memoize
-        //recursion mein hamesha ek extra function bananan padta ahi
-        // vector <int> memo(45+1,-1);
-        // int ans=recursion(n,memo);
-        // return ans;
-
-
-
-        //dp bottom up appraoch 
-        // vector <int> memo(45+1,-1);
-        // int ans=dp(n,memo);
-        // return ans;
-
-
-         //space optimization by keeping only 2 pointers  prev and curr
          if(n<=2)
-         return n;
-         
+         return memo[n]=n;
 
-         int prevprev=1;
-         int prev=1;
-         int curr=2;
-         for(int i=3;i<=n;i++)
-         {  
-            prevprev=curr;  
-            curr=prev+curr;
-            prev=prevprev;
+         return memo[n]=recursion(n-1,memo)+recursion(n-2,memo);
+    }
+    int climbStairs(int n) {
+        //  if(n<=2)
+        //  return n;
 
-         }
-         return curr;
 
-            
+        //  return climbStairs(n-1)+climbStairs(n-2);
+
+
+        vector<int> memo(45+1,-1);
+        int ans=recursion(n,memo);
+
+        return ans;
     }
 };
